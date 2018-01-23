328bea714dc150e1dadbf7a446c6cb083a2b7ef8
cmchang <d05921027@ntu.edu.tw>
2018-01-23 19:51:26 +0800

    add initialize_uninitialized

--- train.py
+++ train.py
@@ -50,6 +50,14 @@ def train(FLAG):
     
     checkpoint_path = os.path.join(FLAG.save_dir, 'model.ckpt')
 
+    # useful function
+    def initialize_uninitialized(sess):
+        global_vars = tf.global_variables()
+        is_not_initialized = sess.run([tf.is_variable_initialized(var) for var in global_vars])
+        not_initialized_vars = [v for (v,f) in zip(global_vars, is_not_initialized) if not f]
+        if len(not_initialized_vars): 
+                sess.run(tf.variables_initializer(not_initialized_vars))
+
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
 
