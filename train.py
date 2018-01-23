daf0a5796bcee47b8c35609f98a54174efc9b556
cmchang <d05921027@ntu.edu.tw>
2018-01-23 19:39:45 +0800

    update train.py with new vgg16

--- train.py
+++ train.py
@@ -8,17 +8,17 @@ from IPython.display import display
 from vgg16 import VGG16
 from utils import CIFAR10, CIFAR100
 
-def main()
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--save_dir', type=str, default='save', help='directory to store checkpointed models')
-	parser.add_argument('--dataset', type=str, default='CIFAR-10', help='dataset in use')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save_dir', type=str, default='save', help='directory to store checkpointed models')
+    parser.add_argument('--dataset', type=str, default='CIFAR-10', help='dataset in use')
     parser.add_argument('--prof_type', type=str, default='all-one', help='type of profile coefficient')
     parser.add_argument('--atp', type=int, default=0, help='alternative training procedure')
     parser.add_argument('--log_dir', type=str, default='log', help='directory containing log text')
     parser.add_argument('--note', type=str, default='', help='argument for taking notes')
-
-	FLAG = parser.parse_args()
-	train(FLAG)
+    
+    FLAG = parser.parse_args()
+    train(FLAG)
 
 def train(FLAG):
     print("Reading dataset...")
@@ -36,30 +36,38 @@ def train(FLAG):
 
     print("Build VGG16 models...")
     dp = [(i+1)*0.05 for i in range(1,20)]
-    vgg16 = VGG16("/home/cmchang/IDP_CNN/vgg16.npy")
+    vgg16 = VGG16("/home/cmchang/IDP_CNN/vgg16.npy", prof_type=FLAG.prof_type)
 
-    # build model using 
-    vgg16.build(dp=dp, prof_type=FLAG.prof_type)
+    # build model using  dp
+    vgg16.build(dp=dp)
 
-    saver = tf.train.Saver(tf.global_variables())
-    tasks = ['100', '70', '40', '10']
-
-    # initial task
-    obj = vgg16.loss_dict[tasks[0]]
+    # define tasks
+    tasks = ['100', '90', '80', '70', '60', '50', '40', '30', '20']
+    print(tasks)
+    
+    saver = tf.train.Saver(tf.global_variables(), max_to_keep=len(tasks))
+    
+    checkpoint_path = os.path.join('/data/put_data/cmchang/save_linear_no_alpha/', 'model.ckpt')
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        
+
         # hyper parameters
-        learning_rate = 2e-4
+        learning_rate = 2e-3
         batch_size = 32
         alpha = 0.5
-        early_stop_patience = 4
+        early_stop_patience = 3
         min_delta = 0.0001
+
+        # optimizer
+        opt = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
 
         # recorder
         epoch_counter = 0
-        
+
+        # tensorboard writer
+        writer = tf.summary.FileWriter("log/", sess.graph)
+
         # progress bar
         ptrain = IntProgress()
         pval = IntProgress()
@@ -68,36 +76,37 @@ def train(FLAG):
         ptrain.max = int(Xtrain.shape[0]/batch_size)
         pval.max = int(Xtest.shape[0]/batch_size)
         
-        # optimizer
-        opt = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9)
-        
-        # tensorboard writer
-        writer = tf.summary.FileWriter(FLAG.log_dir+FLAG.note, sess.graph)
-
+        # initial task
+        obj = vgg16.loss_dict[tasks[0]]
+    
         while(len(tasks)):
-        
+
             # acquire a new task
             cur_task = tasks[0]
             tasks = tasks[1:]
             new_obj = vgg16.loss_dict[cur_task]
-            
-            # task-wise loss aggregation
-            obj = tf.add(tf.multiply(obj, 1.0-alpha), tf.multiply(new_obj, alpha))
-            
+
+            # just finished a task
+            if epoch_counter > 0:
+                # save models
+                saver.save(sess, checkpoint_path, global_step=epoch_counter)
+
+                # task-wise loss aggregation
+                obj = tf.add(tf.multiply(1-alpha,obj), tf.multiply(alpha,new_obj))
+
             # optimizer
             train_op = opt.minimize(obj)
-            
+
             # re-initialize
             initialize_uninitialized(sess)
-            
+
             # reset due to adding a new task
             patience_counter = 0
             current_best_val_loss = 100000 # a large number
-            
+
             # optimize when the aggregated obj
             while(patience_counter < early_stop_patience):
                 stime = time.time()
-
                 bar_train = Bar('Training', max=int(Xtrain.shape[0]/batch_size), suffix='%(index)d/%(max)d - %(percent).1f%% - %(eta)ds')
                 bar_val =  Bar('Validation', max=int(Xtest.shape[0]/batch_size), suffix='%(index)d/%(max)d - %(percent).1f%% - %(eta)ds')
                 
@@ -110,46 +119,43 @@ def train(FLAG):
                     ptrain.value +=1
                     ptrain.description = "Training %s/%s" % (i, ptrain.max)
                     bar_train.next()
-                
+
                 # validation
                 val_loss = 0
                 val_accu = 0
-                for i in range(int(Xtest.shape[0]/batch_size)):
-                    st = i*batch_size
-                    ed = (i+1)*batch_size
+                for i in range(int(Xtest.shape[0]/200)):
+                    st = i*200
+                    ed = (i+1)*200
                     loss, accu, epoch_summary = sess.run([obj, vgg16.accu_dict[cur_task], vgg16.summary_op],
                                         feed_dict={vgg16.x: Xtest[st:ed,:],
-                                                vgg16.y: Ytest[st:ed,:]})
+                                                   vgg16.y: Ytest[st:ed,:]})
                     val_loss += loss
                     val_accu += accu
                     pval.value += 1
-                    pval.description = "Testing %s/%s" % (i, pval.max)
+                    pval.description = "Testing %s/%s" % (i, pval.value)
+                val_loss = val_loss/pval.value
+                val_accu = val_accu/pval.value
 
-                val_loss = val_loss/pval.max
-                val_accu = val_accu/pval.max
-                
                 # early stopping check
                 if (current_best_val_loss - val_loss) > min_delta:
                     current_best_val_loss = val_loss
                     patience_counter = 0
-                    checkpoint_path = os.path.join(FLAG.save_dir+FLAG.note, 'model.ckpt')
-                    saver.save(sess, checkpoint_path, global_step=epoch_counter)
                 else:
                     patience_counter += 1
-                
+
                 # shuffle Xtrain and Ytrain in the next epoch
                 idx = np.random.permutation(Xtrain.shape[0])
                 Xtrain, Ytrain = Xtrain[idx,:,:,:], Ytrain[idx,:]
-                
+
                 # epoch end
                 writer.add_summary(epoch_summary, epoch_counter)
                 epoch_counter += 1
-                
+
                 ptrain.value = 0
                 pval.value = 0
                 bar_train.finish()
                 bar_val.finish()
-                
+
                 print("Epoch %s (%s), %s sec >> obj loss: %.4f, task at %s: %.4f" % (epoch_counter, patience_counter, round(time.time()-stime,2), val_loss, cur_task, val_accu))
 
         writer.close()
