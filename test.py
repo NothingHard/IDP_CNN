64c4bcff4bc16d76392a931b82cdd4d6204f46f5
cmchang <twcmchang@gmail.com>
2018-01-25 20:47:51 +0800

    run all checkpoints in order

--- test.py
+++ test.py
@@ -41,21 +41,29 @@ def test(FLAG):
             sess.run(tf.global_variables_initializer())
             saver = tf.train.Saver()
             ckpt = tf.train.get_checkpoint_state(FLAG.save_dir)
+            
             if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
-                print("Model restored %s" % ckpt.model_checkpoint_path)
-                sess.run(tf.global_variables())
-            print("Initialized")
+                count = 0
+                for checkpoint in all_model_checkpoint_paths:
+                    saver.restore(sess, checkpoint)
+                    print("Model restored %s" % checkpoint)
+                    sess.run(tf.global_variables())
+                    print("Initialized")
 
-            output = []
-            for dp_i in dp:
-                accu = sess.run(vgg16.accu_dict[str(int(dp_i*100))], feed_dict={vgg16.x: Xtest[:5000,:], vgg16.y: Ytest[:5000,:]})
-                accu2 = sess.run(vgg16.accu_dict[str(int(dp_i*100))], feed_dict={vgg16.x: Xtest[5000:,:], vgg16.y: Ytest[5000:,:]})
-                output.append((accu+accu2)/2)
-                print("At DP={dp:.4f}, accu={perf:.4f}".format(dp=dp_i, perf=(accu+accu2)/2))
-            res = pd.DataFrame.from_dict({'DP':[int(dp_i*100) for dp_i in dp],'accu':output})
-            res.to_csv(FLAG.output, index=False)
-            print("Write into %s" % FLAG.output)
+            #     saver.restore(sess, ckpt.model_checkpoint_path)
+            #     print("Model restored %s" % ckpt.model_checkpoint_path)
+            #     sess.run(tf.global_variables())
+            # print("Initialized")
+                    count += 1
+                    output = []
+                    for dp_i in dp:
+                        accu = sess.run(vgg16.accu_dict[str(int(dp_i*100))], feed_dict={vgg16.x: Xtest[:5000,:], vgg16.y: Ytest[:5000,:]})
+                        accu2 = sess.run(vgg16.accu_dict[str(int(dp_i*100))], feed_dict={vgg16.x: Xtest[5000:,:], vgg16.y: Ytest[5000:,:]})
+                        output.append((accu+accu2)/2)
+                        print("At DP={dp:.4f}, accu={perf:.4f}".format(dp=dp_i, perf=(accu+accu2)/2))
+                    res = pd.DataFrame.from_dict({'DP':[int(dp_i*100) for dp_i in dp],'accu':output})
+                    res.to_csv("task%s_%s" % (FLAG.output, count), index=False)
+                    print("Write into task%s_%s" % (count, FLAG.output))
 
 if __name__ == '__main__':
     main()
